b2ee380af9775b1cbfe898afc83a1e6b961faa70
Claes Redestad <claes.redestad@oracle.com>
2021-03-15 13:02:49 +0100

    Rename and move benchmark

--- test/micro/org/openjdk/bench/java/util/StringJoinerBenchmark.java
+++ test/micro/org/openjdk/bench/java/util/StringJoinerBenchmark.java
@@ -20,19 +20,17 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package org.openjdk.bench.java.lang;
+package org.openjdk.bench.java.util;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
 
 import java.util.StringJoiner;
 import java.util.concurrent.ThreadLocalRandom;
