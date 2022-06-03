ae3b5f30c1ce62f05e7c250b6bfc89f99bb72d2b
Claes Redestad <claes.redestad@oracle.com>
2022-06-03 10:32:54 +0200

    StringConcatFactory: Apply stringifiers separately to make better use of repeated filter optimization

--- src/java.base/share/classes/java/lang/invoke/StringConcatFactory.java
+++ src/java.base/share/classes/java/lang/invoke/StringConcatFactory.java
@@ -480,26 +480,32 @@ public final class StringConcatFactory {
         // The filtered argument type list is used all over in the combinators below.
 
         Class<?>[] ptypes = mt.erase().parameterArray();
-        MethodHandle[] filters = null;
+        MethodHandle[] objectFilters = null;
+        MethodHandle[] floatFilters = null;
+        MethodHandle[] doubleFilters = null;
         for (int i = 0; i < ptypes.length; i++) {
             Class<?> cl = ptypes[i];
-            MethodHandle filter = null;
             if (cl == byte.class || cl == short.class) {
                 // use int for subword integral types; still need special mixers
                 // and prependers for char, boolean
                 ptypes[i] = int.class;
             } else if (cl == Object.class) {
-                filter = objectStringifier();
-            } else if (cl == float.class) {
-                filter = floatStringifier();
-            } else if (cl == double.class) {
-                filter = doubleStringifier();
-            }
-            if (filter != null) {
-                if (filters == null) {
-                    filters = new MethodHandle[ptypes.length];
+                if (objectFilters == null) {
+                    objectFilters = new MethodHandle[ptypes.length];
                 }
-                filters[i] = filter;
+                objectFilters[i] = objectStringifier();
+                ptypes[i] = String.class;
+            } else if (cl == float.class) {
+                if (floatFilters == null) {
+                    floatFilters = new MethodHandle[ptypes.length];
+                }
+                floatFilters[i] = floatStringifier();
+                ptypes[i] = String.class;
+            } else if (cl == double.class) {
+                if (doubleFilters == null) {
+                    doubleFilters = new MethodHandle[ptypes.length];
+                }
+                doubleFilters[i] = doubleStringifier();
                 ptypes[i] = String.class;
             }
         }
@@ -567,8 +573,14 @@ public final class StringConcatFactory {
         // The method handle shape here is (<args>).
 
         // Apply filters, converting the arguments:
-        if (filters != null) {
-            mh = MethodHandles.filterArguments(mh, 0, filters);
+        if (objectFilters != null) {
+            mh = MethodHandles.filterArguments(mh, 0, objectFilters);
+        }
+        if (floatFilters != null) {
+            mh = MethodHandles.filterArguments(mh, 0, floatFilters);
+        }
+        if (doubleFilters != null) {
+            mh = MethodHandles.filterArguments(mh, 0, doubleFilters);
         }
 
         return mh;
