b6178a176aa4f5761c288a8c726047056ac35924
redestad <claes.redestad@oracle.com>
2024-04-24 11:45:43 +0200

    Simplify ClassDesc.ofDescriptor and ReferenceClassDescImpl constructor

--- src/java.base/share/classes/java/lang/constant/ClassDesc.java
+++ src/java.base/share/classes/java/lang/constant/ClassDesc.java
@@ -158,20 +158,10 @@ public sealed interface ClassDesc
      * @see ClassDesc#ofInternalName(String)
      */
     static ClassDesc ofDescriptor(String descriptor) {
-        requireNonNull(descriptor);
-        if (descriptor.isEmpty()) {
-            throw new IllegalArgumentException(
-                    "not a valid reference type descriptor: " + descriptor);
+        if (descriptor.length() == 1) {
+            return Wrapper.forBasicType(descriptor.charAt(0)).primitiveClassDescriptor();
         }
-        int depth = ConstantUtils.arrayDepth(descriptor);
-        if (depth > ConstantUtils.MAX_ARRAY_TYPE_DESC_DIMENSIONS) {
-            throw new IllegalArgumentException(
-                    "Cannot create an array type descriptor with more than " +
-                    ConstantUtils.MAX_ARRAY_TYPE_DESC_DIMENSIONS + " dimensions");
-        }
-        return (descriptor.length() == 1)
-               ? Wrapper.forBasicType(descriptor.charAt(0)).primitiveClassDescriptor()
-               : new ReferenceClassDescImpl(descriptor);
+        return new ReferenceClassDescImpl(descriptor);
     }
 
     /**
